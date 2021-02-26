21760b1802f1c37c4b7402b3e5b1027f1d8bf13c
Robin Quint <rob2309@hotmail.de>
2021-02-26 17:01:25 +0100

    Kind of fixed resizing (camera aspect ratio not updating yet)

--- src/core/window.rs
+++ src/core/window.rs
@@ -80,8 +80,7 @@ pub fn start(engine_init: EngineInit) -> ! {
                 }
                 _ => {}
             },
-            Event::MainEventsCleared => {}
-            Event::RedrawRequested(_) => {
+            Event::MainEventsCleared => {
                 engine
                     .gameloop
                     .update(&mut engine.vulkan_manager, &engine.scene);

f054f12435f19f358ce7c10d95ef16bf251a166f
Michael Lohr <michael@lohr-ffb.de>
2021-03-02 19:16:22 +0100

    Started triangle example

--- examples/minimal/main.rs
+++ examples/minimal/main.rs
@@ -1,11 +1,33 @@
 /// A minimal example that just initializes the engine but does not display anything
 use std::process::exit;
 
+use crystal::prelude::{Mat4, Quaternion, Vec2, Vec3, Vec4};
 use log::error;
 use vulkan_engine::{
     core::window::{self, Dimensions},
-    engine::{self, EngineInit},
+    engine::{self, Engine, EngineInit},
+    scene::{
+        camera::Camera,
+        light::DirectionalLight,
+        material::{MaterialBinding, MaterialBindingFragment, MaterialData, MaterialPipeline},
+        model::{
+            mesh::{Face, MeshData, Submesh, Vertex},
+            Model,
+        },
+        transform::Transform,
+    },
 };
+
+#[repr(C)]
+#[derive(MaterialBindingFragment)]
+struct VertexColorData {
+    color: Vec4<f32>,
+}
+
+#[derive(MaterialData)]
+struct VertexMaterialData {
+    color_data: VertexColorData,
+}
 
 fn main() {
     // setting up logger
@@ -28,7 +50,8 @@ fn main() {
 
     // start engine
     match engine_init {
-        Ok(engine_init) => {
+        Ok(mut engine_init) => {
+            setup(&mut engine_init.engine);
             engine_init.start();
         }
         Err(err) => {
@@ -36,4 +59,94 @@ fn main() {
             exit(1);
         }
     }
+}
+
+fn setup(engine: &mut Engine) {
+    let scene = &mut engine.scene;
+
+    let pipeline = MaterialPipeline::<VertexMaterialData>::new(
+        engine.vulkan_manager.device.clone(),
+        (*engine.vulkan_manager.allocator).clone(),
+        "vertex_unlit",
+        engine.vulkan_manager.desc_layout_frame_data,
+        engine.vulkan_manager.renderpass,
+        engine.vulkan_manager.swapchain.extent.width,
+        engine.vulkan_manager.swapchain.extent.height,
+    )
+    .unwrap();
+    let material0 = pipeline
+        .create_material(VertexMaterialData {
+            color_data: VertexColorData {
+                color: Vec4::new(0.5, 0.5, 0.5, 1.0),
+            },
+        })
+        .unwrap();
+
+    // create triangle
+    let mesh_data = MeshData {
+        vertices: vec![
+            Vertex {
+                position: Vec3::new(-1.0, -1.0, 0.0),
+                color: Vec3::new(1.0, 0.0, 1.0),
+                normal: Vec3::new(0.0, 0.0, -1.0),
+                uv: Vec2::new(0.0, 0.0),
+            },
+            Vertex {
+                position: Vec3::new(1.0, -1.0, 0.0),
+                color: Vec3::new(1.0, 0.0, 1.0),
+                normal: Vec3::new(0.0, 0.0, -1.0),
+                uv: Vec2::new(0.0, 0.0),
+            },
+            Vertex {
+                position: Vec3::new(0.0, 1.0, 0.0),
+                color: Vec3::new(1.0, 0.0, 1.0),
+                normal: Vec3::new(0.0, 0.0, -1.0),
+                uv: Vec2::new(0.0, 0.0),
+            },
+        ],
+        submeshes: vec![Submesh {
+            faces: vec![Face { indices: [0, 1, 2] }],
+        }],
+    };
+
+    let transform = Mat4::translate(Vec3::new(0.0, 0.0, 5.0));
+    let inv_transform = Mat4::translate(Vec3::new(0.0, 0.0, -5.0));
+    let mesh = mesh_data
+        .bake(
+            (*engine.vulkan_manager.allocator).clone(),
+            transform,
+            inv_transform,
+        )
+        .unwrap();
+
+    scene.add(Model {
+        material: material0,
+        mesh: mesh,
+        transform: Transform {
+            position: Vec3::new(0.0, 0.0, 0.0),
+            rotation: Quaternion::new(0.0, 0.0, 0.0, 1.0),
+            scale: Vec3::new(1.0, 1.0, 1.0),
+        },
+    });
+
+    // let lights = &mut scene.light_manager;
+    // lights.add_light(DirectionalLight {
+    //     direction: Vec3::new(0., -1., 0.),
+    //     illuminance: Vec3::new(10.1, 10.1, 10.1),
+    // });
+
+    // setup camera
+    Camera::builder()
+        //.fovy(30.0.deg())
+        .position(Vec3::new(0.0, 0.0, -5.0))
+        .aspect(
+            engine.info.window_info.initial_dimensions.width as f32
+                / engine.info.window_info.initial_dimensions.height as f32,
+        )
+        .build()
+        .update_buffer(
+            &engine.vulkan_manager.allocator,
+            &mut engine.vulkan_manager.uniform_buffer,
+            0,
+        );
 }
---
+++ shaders/vertex_unlit.glsl
@@ -0,0 +1,33 @@
+//# NAME Vertex Unlit
+//# DESCRIPTION Renders vertex colors without lighting
+//# VERSION 450
+
+//# TYPE VERTEX
+layout (location = 0) in vec3 i_position;
+layout (location = 1) in vec3 i_color;
+layout (location = 2) in vec3 i_normal;
+layout (location = 4) in mat4 i_modelMatrix;
+layout (location = 8) in mat4 i_inverseModelMatrix;
+
+layout (set = 0, binding = 0) uniform FrameData {
+    mat4 view_matrix;
+    mat4 projection_matrix;
+    vec3 cam_pos;
+} u_FrameData;
+
+layout (location = 0) out vec3 o_color;
+layout (location = 1) out vec3 o_normal;
+
+void main() {
+    gl_Position = u_FrameData.projection_matrix * u_FrameData.view_matrix * i_modelMatrix * vec4(i_position, 1.0);
+    o_color = i_color;
+}
+
+//# TYPE FRAGMENT
+layout (location = 0) in vec3 i_color;
+
+layout (location = 0) out vec4 o_color;
+
+void main(){
+	o_color = i_color;
+}

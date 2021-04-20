947827f95a0092ce5922014be400cd6b50fa54bb
Michael Lohr <michael@lohr.dev>
2021-04-20 18:26:14 +0200

    Improve error handling

--- src/vulkan/mod.rs
+++ src/vulkan/mod.rs
@@ -89,12 +89,7 @@ impl VulkanManager {
         window: &winit::window::Window,
         max_frames_in_flight: u8,
     ) -> GraphicsResult<Self> {
-        // box ash::Entry to anyhow::error
-        fn init_entrypoint() -> anyhow::Result<ash::Entry> {
-            Ok(ash::Entry::new()?)
-        }
-
-        let entry = init_entrypoint()?;
+        let entry = ash::Entry::new().map_err(anyhow::Error::from)?;
         let instance = VulkanManager::init_instance(engine_info, &entry, &window)?;
 
         let debug = DebugMessenger::init(&entry, &instance)?;

7b7a317fd104856785cca3fae34d95ed557345b5
Robin Quint <rob2309@hotmail.de>
2021-12-28 21:51:50 +0100

    Fixed build script to not rerun everytime

--- crates/engine/build.rs
+++ crates/engine/build.rs
@@ -4,7 +4,7 @@ use std::process::Command;
 
 fn main() {
     // Tell Cargo that if the given file changes, to rerun this build script.
-    println!("cargo:rerun-if-changed=shaders/");
+    println!("cargo:rerun-if-changed=../../shaders/");
 
     let engine_dir = env::current_dir().unwrap(); // .
     let project_dir = engine_dir.parent().unwrap().parent().unwrap(); // ../../
